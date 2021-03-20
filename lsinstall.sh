d8632cc5f547529a14dd1cfd24bf90837a9edfe6
Guanyu Wu <lambda@funcptr.org>
2021-03-19 21:07:41 -0400

    Initial work on ubuntu. Managed to get bcachefs installing.

--- lsinstall.sh
+++ lsinstall.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-if [[ -z $1 || -z $2 ]]; then
+
+echo -n "Install target: "
+read TARGET;
+
+if [[ -z $TARGET ]]; then
     echo "No install target specified!"
     exit -1;
 fi
 
-DISTRO=$(ssh -i install/ssh $1@$2 "source /etc/os-release; echo \$ID")
+echo -n "Install user: "
+read USER;
 
-ansible-playbook --private-key install/ssh "playbooks/lstor-deploy-$DISTRO.yaml" --extra-vars "ansible_user=$1 target=$2" -i "$2,"
+echo -n "User password for sudo escelation (leave blank if none exists): "
+read -s PASSWORD;
+echo ""
+
+DISTRO=$(ssh -i install/ssh $USER@$TARGET "source /etc/os-release; echo \$ID")
+
+if [[ ! -e "playbooks/lstor-deploy-$DISTRO.yaml" ]]; then
+    echo "Unsupported distro: $DISTRO";
+    exit -1;
+fi
+
+ansible-playbook --private-key install/ssh "playbooks/lstor-deploy-$DISTRO.yaml" --extra-vars "ansible_user=$USER lstor_target=$TARGET ansible_become_password=$PASSWORD" -i "$TARGET,"
